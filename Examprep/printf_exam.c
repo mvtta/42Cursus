a008c090189f913cbd6c8c21612532e41f49b50c
Mariana Valdetaro <mvaldeta@c1r21s5.42lisboa.com>
2021-10-15 18:30:18 +0100

    update push_swap

---
+++ Examprep/printf_exam.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_exam.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvaldeta <mvaldeta@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/13 16:13:09 by mvaldeta          #+#    #+#             */
+/*   Updated: 2021/08/24 12:50:09 by mvaldeta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+{
+    type_c  
+    type_d  
+    type_x
+    width
+    prec  
+}
+
+/* 
+1.      pre-process       : print until %;
+4.      process block     : initializes struct with identfiers for formating ie: width = 10;
+2.      type block        : %d initized ie: type d = 1;
+3.      format print unit : 
+*/
+
+
+char *itoa_base (int num , int base)
+{
+    char hex[16] = "0123456789abcdef"
+    while (n)
+        res[i] = hex[num % base]
+}
+
+
+va_args(long, hex)
+
